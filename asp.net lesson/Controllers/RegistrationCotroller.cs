c9a22c35facb00299a1a7a18a727e67ea69a3aae
dimakozlovich <kozlovichdima21@gmail.com>
2022-12-14 16:53:13 +0300

    Add registration

---
+++ asp.net lesson/Controllers/RegistrationCotroller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp.net_lesson.Controllers
+{
+    public class RegistrationCotroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
